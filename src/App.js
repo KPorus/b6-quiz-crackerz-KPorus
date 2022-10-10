f9953f960f0b75cca4765edd8218cff75013261d
Fardin Khan_451 <designerfardin2021@gmail.com>
2022-10-10 23:40:04 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Chart from './component/Chart';
+
+function App() {
+  return (
+    <div className="App">
+      <Chart></Chart>
+    </div>
+  );
+}
+
+export default App;
