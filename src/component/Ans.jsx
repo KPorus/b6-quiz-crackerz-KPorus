536cf9e5c960abc01768889da75f4065d309c737
Fardin Khan_451 <designerfardin2021@gmail.com>
2022-10-12 17:52:53 +0600

    sweet-alert

--- src/component/Ans.jsx
+++ src/component/Ans.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
-
+import Swal from 'sweetalert2'
+import { useState } from 'react';
 const Ans = ({options,ans}) => {
-    console.log(ans);
-    let a = document.getElementById('re')
-    console.log(a)
+   console.log(options);
+    const [state, setstate] = useState(true);
+    let ansClick = (state)=>
+    {
+        
+        if(state)
+        {
+            if(options === ans)
+            {
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Nicely Done',
+                    icon: 'success',
+                  })
+            }else{
+                Swal.fire({
+                    title: 'Error!',
+                    text: 'Do you want to continue',
+                    icon: 'error',
+                    confirmButtonText: 'try Again'
+                  })
+            }
+        }
+    }
     return (
-        <div className='border border-teal-700 m-2'>
-            <ul className='text-xl p-2'>
-                <li><input className='mr-3' type="radio" id="re"  name="radAnswer" value={options}/>
-                <label for="re">{options}</label></li>
+        <div  onClick={()=>ansClick(state)} className={`border border-teal-700 m-2 drop-shadow-xl `}>
+           <button >
+           <ul className='text-xl p-2'> 
+                <span id="answ">{options}</span>
             </ul>
+           </button>
         </div>
     );
 };
